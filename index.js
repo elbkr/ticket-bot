79d3aee06fe62ea7555eeb4267b22057875d3fe2
Sadness <95174481+unf6@users.noreply.github.com>
2023-07-27 21:48:12 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config(); //{path: "dev.env"} if developing
-
-/* Import our client structure */
-const Bot = require("./src/struct/Bot");
-const client = new Bot();
-
-/* Call our start function to load the bot instance */
-(async () => await client.start(process.env.TOKEN))();
